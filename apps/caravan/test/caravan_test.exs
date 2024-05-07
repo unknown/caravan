426768c8b6af02e06ee37b49e1ce8b5b37519ea3
David Mo <44306450+unknown@users.noreply.github.com>
2024-05-07 15:34:52 -0400

    Add test with 1000 tasks

--- apps/caravan/test/caravan_test.exs
+++ apps/caravan/test/caravan_test.exs
@@ -47,4 +47,44 @@ defmodule CaravanTest do
   after
     Emulation.terminate()
   end
+
+  test "Caravan responds to 1000 commands" do
+    Emulation.init()
+
+    nodes = [:a, :b, :c]
+    config = Caravan.new_configuration(nodes)
+
+    Emulation.spawn(:server, fn -> Caravan.run(config) end)
+
+    nodes
+    |> Enum.map(fn x ->
+      Emulation.spawn(x, fn ->
+        config = Caravan.Worker.new()
+        Caravan.Worker.run(config)
+      end)
+    end)
+
+    client =
+      Emulation.spawn(:client, fn ->
+        for x <- 1..1000 do
+          Emulation.send(:server, Caravan.Task.new(:double, x))
+        end
+
+        for _ <- 1..1000 do
+          receive do
+            {:server, _} -> nil
+          end
+        end
+      end)
+
+    handle = Process.monitor(client)
+
+    receive do
+      {:DOWN, ^handle, _, _, _} -> true
+    after
+      30_000 -> assert false
+    end
+  after
+    Emulation.terminate()
+  end
 end
